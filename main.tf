633e039283c59789f0b25356b2fbe7e9dff9f735
Kenneth Aponte <53104206+whatskendoing@users.noreply.github.com>
2025-08-12 06:31:18 -0500

    udpated aws_security_group_rule command

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group"  "blog" {
 
 }
 
-resource "aws_security_rule" "blog_http_in" {
+resource "aaws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -47,7 +47,7 @@ resource "aws_security_rule" "blog_http_in" {
 
 }
 
-resource "aws_security_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -58,7 +58,7 @@ resource "aws_security_rule" "blog_https_in" {
 
 }
 
-resource "aws_security_rule" "blog_everythign_out" {
+resource "aws_security_group_rule" "blog_everythign_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
