7ebcc1853d627be0ee8553123d49d4e30357005b
Kenneth Aponte <53104206+whatskendoing@users.noreply.github.com>
2025-08-12 08:23:48 -0500

    fixes

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
+# Remove the terraform and provider blocks from here
 data "aws_ami" "app_ami" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["979382823631"] # Bitnami
 }
 
@@ -19,20 +17,20 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "HelloWorld"
   }
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
-  tags = {
-    Terraform = "true"
-  }
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out"
+
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -42,9 +40,9 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = aws_security_group.blog.id
 }
-
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
@@ -52,9 +50,9 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = aws_security_group.blog.id
 }
-
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
@@ -62,5 +60,17 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+# Add Tomcat port for Bitnami AMI
+resource "aws_security_group_rule" "blog_tomcat_in" {
+  type        = "ingress"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = aws_security_group.blog.id
 }
